f1fa2f3eff10777079440c0db7d44014d6e3066a
Mazy Lawzey <87779640+MazyLawzey@users.noreply.github.com>
2026-03-27 23:47:12 +0300

    Add SHA-256 hashing functionality
    Implement SHA-256 hashing in C with user input.

---
+++ Sha256_hashing.c
@@ -0,0 +1,23 @@
+#include "sha256.h"
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char text[256];
+    printf("введите текст для хэширования: ");
+    scanf("%255s", text);
+    BYTE hash[SHA256_BLOCK_SIZE];
+    SHA256_CTX ctx;
+
+    sha256_init(&ctx);
+    sha256_update(&ctx, (const BYTE *)text, strlen(text));
+    sha256_final(&ctx, hash);
+
+    printf("SHA-256 hash: ");
+    for (int i = 0; i < SHA256_BLOCK_SIZE; i++) {
+        printf("%02x", hash[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
