b3d062b4616dc5692c52538531e3de842fd70d73
Mazy Lawzey <87779640+MazyLawzey@users.noreply.github.com>
2026-04-29 22:17:06 +0300

    Add basic FBI hacking simulation in fbi.c

---
+++ fbi.c
@@ -0,0 +1,11 @@
+// hack the fbi
+#include <stdio.h>
+
+int main() {
+    int percents;
+    printf("Hakcing FBI: %d", percents);
+
+    for (int percents = 1; percents <= 100; percents++) {
+        printf("Hakcing FBI: %d\n", percents);
+    }
+}
